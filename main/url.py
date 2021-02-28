8a7c93594527c95a1fe384eee562826fb2dc16ed
gersans <71356535+gersans@users.noreply.github.com>
2021-02-28 11:44:28 -0500

    Update url.py

--- main/url.py
+++ main/url.py
@@ -5,29 +5,28 @@ from bs4 import BeautifulSoup
 from dearpygui.core import *
 from dearpygui.simple import *
 
+#generate url based on anilist max characters
 def urlGenerator():
     urlID = random.randint(1,200000)
-    return ''.join(str(urlID))
+    return (str(urlID)
 
+#grab character name from anilist            
 def charName():
     url='https://anilist.co/character/' + urlGenerator()
-
+    
+    #response from url
     resp=requests.get(url)
 
+    #if response is okay call soup to inspect html
     if resp.status_code==200:
-        print("Success")
         soup=BeautifulSoup(resp.text,'html.parser')
 
+        #if soup finds and does not equal None set name to what was found
         if soup.find("h1",{"class":"name"})!=None:
             name=soup.find("h1",{"class":"name"})
-            print(name.text)
             return name.text
-        else:
-            print("Invalid ID")
 
-    else:
-        print("error")
-
+# makes list based on text found in charName()
 def makeList():
     names = None
     characterName = charName()
@@ -35,6 +34,4 @@ def makeList():
     if characterName != None:
         names = characterName.split()
         return names
-    else:
-        print('Generate new url')
 
